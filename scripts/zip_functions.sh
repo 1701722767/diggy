4186f6d0836608f9c9e31e481d68948500ff5880
Juan Giraldo <jgiraldo@truora.com>
2022-04-04 18:28:23 -0500

    add libs in zip

--- scripts/zip_functions.sh
+++ scripts/zip_functions.sh
@@ -15,6 +15,17 @@ for d in ./services/* ; do
     zip_name="${d##*/}-service_${f##*/}"
 
     zip -jr ./artifacts/$zip_name.zip $f/main.py
+    # pip freeze > $f/requirements.txt
+    # pip install -r $f/requirements.txt -t ./package
+    # zip -jr ./artifacts/$zip_name.zip $f/package
+    input=$f/requirements.txt
+    while IFS= read -r line
+    do
+      pip install --target ./package $line
+    done < "$input"
+
+    zip -jr ./artifacts/$zip_name.zip $f/package
+
     printf '@ main.py\n@=lambda_function.py\n' | zipnote -w ./artifacts/$zip_name.zip
   done
 done
