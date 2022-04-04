c20f642c120ee468651413f452dec743d0c878af
Juan Giraldo <jgiraldo@truora.com>
2022-04-04 11:06:25 -0500

    start test for python

---
+++ services/sample-service/main.py
@@ -0,0 +1,30 @@
+
+from ast import Try
+class Request:
+    """
+    Request is the class used for handling the request
+    """
+    def __init__(self):
+        self.err = None
+
+    def process(self):
+        print("hello world")
+
+
+
+def lambda_handler(event, context):
+    """
+    lambda_handler function that starts the lambda
+    """
+    _ = context
+    req = Request()
+    try:
+        req.process()
+
+    except Exception as err:
+        print(err)
+    finally:
+        if req.err is None:
+            return "all is good"
+        else:
+            return "all is not good"
