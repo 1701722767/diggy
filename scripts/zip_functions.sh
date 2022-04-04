1428b95eac7abf21e1fbbaba317612933e4e1db2
Juan Giraldo <jgiraldo@truora.com>
2022-04-04 16:40:28 -0500

    create a .sh file

---
+++ scripts/zip_functions.sh
@@ -0,0 +1,17 @@
+mkdir -p './artifacts'
+
+for d in ../services/* ; do
+  if [ ! -d "$d" ]; then
+    continue
+  fi
+
+  for f in $d/functions/* ; do
+    if [ ! -d "$f" ]; then
+      continue
+    fi
+
+    echo "$f"
+    zip -jr ../artifacts/sample-service_sample.zip $f/main.py
+    printf "@ main.py\n@=lambda_function.py\n" | zipnote -w ../artifacts/sample-service_sample.zip
+  done
+done
