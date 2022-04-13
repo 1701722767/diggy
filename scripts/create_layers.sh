66b081d5750147441a35907bf8412506829614a2
Juan Giraldo <jgiraldo@truora.com>
2022-04-13 14:21:53 -0500

    install all packages

--- scripts/create_layers.sh
+++ scripts/create_layers.sh
@@ -5,23 +5,13 @@ mkdir -p './artifacts/layers'
 while read -r package; do
     echo "Creating layer for $package"
 
-    mkdir -p ./python
-    pip3 install --target=./python $package
+    # mkdir -p ./python
+    # pip3 install --target=./python $package
 
-    zip_name="${package}_layer"
-    zip -r $zip_name.zip ./python
-    mv $zip_name.zip './artifacts/layers'
+    # zip_name="${package}_layer"
+    # zip -r $zip_name.zip ./python
+    # mv $zip_name.zip './artifacts/layers'
 
-    rm -r ./python
+    # rm -r ./python
 
 done < ./services/requirements.txt
-
-
-
-
-
-
-
-#   cd ../..
-
-# done
--- scripts/zip_functions.sh
+++ scripts/zip_functions.sh
@@ -30,25 +30,3 @@ for d in ./services/* ; do
     printf '@ main.py\n@=lambda_function.py\n' | zipnote -w ./artifacts/functions/$zip_name.zip
   done
 done
-
-# create zip for service layer
-for d in ./services/* ; do
-  if [ ! -d "$d" ]; then
-    continue
-  fi
-
-  mkdir -p $d/python
-  pip3 install --target=$d/python -r $d/requirements.txt
-
-  cd $d
-
-  zip_name="${d##*/}-service_layer"
-  zip -r $zip_name.zip ./python
-  mv $zip_name.zip '../../artifacts/layers'
-
-
-  rm -r ./python
-
-  cd ../..
-
-done
