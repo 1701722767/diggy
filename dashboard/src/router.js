55047578de533b38e22555463af77b557b9551da
sergiomonz <sergio.1701510237@ucaldas.edu.co>
2022-05-11 18:40:18 -0500

    Fixing router file

--- dashboard/src/router.js
+++ dashboard/src/router.js
@@ -1,35 +1,38 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-import Login from './components/Login.vue'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Login from './components/Login.vue';
+import HelloWorld from './components/HelloWorld.vue';
 import Register from './components/Register.vue'
 import EventRegister from './components/EventRegister.vue'
 
-Vue.use(Router)
+
+Vue.use(Router);
 
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HelloWorld
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: Login
-    },
-    {
-      path: '/register',
-      name: 'register',
-      component: Register
-    },
-    {
-      path: '/eventregister',
-      name: 'Event-register',
-      component: EventRegister
-    }
-  ]
-})
+
+    routes: [        
+        {
+            path: '/',
+            name: 'home',
+            component: HelloWorld,
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: Login,
+        },
+        {
+          path: '/register',
+          name: 'register',
+          component: Register
+        },
+        {
+          path: '/eventregister',
+          name: 'Event-register',
+          component: EventRegister
+        }
+    ],
+    
+});
