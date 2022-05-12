a1352a3880ddc3fc0f49b4b506106db2db01fc59
nicolascr181 <nicolas.1701625295@ucaldas.edu.co>
2022-05-11 21:09:25 -0500

    resolving conflicts

--- dashboard/src/main.js
+++ dashboard/src/main.js
@@ -1,9 +1,9 @@
-import Vue from "vue";
-import App from "./App.vue";
-import vuetify from "./plugins/vuetify";
+import Vue from 'vue';
+import App from './App.vue';
+import vuetify from './plugins/vuetify';
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getMessaging, getToken, onMessage } from "firebase/messaging";
+import { initializeApp } from 'firebase/app';
+import { getMessaging, getToken, onMessage } from 'firebase/messaging';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -13,12 +13,12 @@ import { getMessaging, getToken, onMessage } from "firebase/messaging";
 
 const firebaseConfig = {
   apiKey: process.env.VUE_APP_API_KEY,
-  authDomain: "encoded-keyword-270113.firebaseapp.com",
-  projectId: "encoded-keyword-270113",
-  storageBucket: "encoded-keyword-270113.appspot.com",
-  messagingSenderId: "607844518648",
-  appId: "1:607844518648:web:430bd204cad711954c090d",
-  measurementId: "G-CN3426KT9V",
+  authDomain: 'encoded-keyword-270113.firebaseapp.com',
+  projectId: 'encoded-keyword-270113',
+  storageBucket: 'encoded-keyword-270113.appspot.com',
+  messagingSenderId: '607844518648',
+  appId: '1:607844518648:web:430bd204cad711954c090d',
+  measurementId: 'G-CN3426KT9V',
 };
 
 // Initialize Firebase
@@ -29,7 +29,7 @@ const messaging = getMessaging(app);
 
 getToken(messaging, {
   vapidKey:
-    "BEP3v3Oq4LO9qgMQuvxm2AtErmVXChzKgSJ_e64xxrK4Pss9p2eclhv0B0q_1R13o_hI95oBKObNSzuUFCnkMQI",
+    'BEP3v3Oq4LO9qgMQuvxm2AtErmVXChzKgSJ_e64xxrK4Pss9p2eclhv0B0q_1R13o_hI95oBKObNSzuUFCnkMQI',
 })
   .then((currentToken) => {
     if (currentToken) {
@@ -37,27 +37,27 @@ getToken(messaging, {
       // ...
       console.log(currentToken);
 
-      const h3 = document.createElement("h3");
+      const h3 = document.createElement('h3');
       const newContent = document.createTextNode(currentToken);
       h3.appendChild(newContent); //añade texto al div creado.
 
-      const app = document.getElementById("token");
+      const app = document.getElementById('token');
       app.appendChild(h3);
     } else {
       // Show permission request UI
       console.log(
-        "No registration token available. Request permission to generate one."
+        'No registration token available. Request permission to generate one.'
       );
       // ...
     }
   })
   .catch((err) => {
-    console.log("An error occurred while retrieving token. ", err);
+    console.log('An error occurred while retrieving token. ', err);
   });
 
 /// Need implementation when page is in foreground!!!!
 onMessage(messaging, (payload) => {
-  console.log("Message received. ", payload);
+  console.log('Message received. ', payload);
   // ...
 });
 
@@ -66,4 +66,4 @@ Vue.config.productionTip = false;
 new Vue({
   vuetify,
   render: (h) => h(App),
-}).$mount("#app");
+}).$mount('#app');
