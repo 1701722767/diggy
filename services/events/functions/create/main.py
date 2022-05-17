8681c9d0f410552fa4033418ff4a41e21ef14441
nicolascr181 <nicolas.1701625295@ucaldas.edu.co>
2022-05-17 09:57:35 -0500

    changes requested completed

--- services/events/functions/create/main.py
+++ services/events/functions/create/main.py
@@ -9,23 +9,27 @@ def create_event(event=None):
     
     # this will search for dynamoDB table 
     
-    response = table.put_item(
-        Item= {
-        "event_id": event['event_id'],
-        "category_id":event['category_id'],
-        "event_name": event['event_name'],
-        "event_coordinates": event['event_coordinates'],
-        "event_images": event['event_images'],
-        "event_description": event['event_description'],
-        "event_range_age": event['event_range_age'],
-        "event_price": event['event_price'],
-        "event_slots": event['event_slots'],
-        "event_max": event['event_max'],
-        "event_datestart": event['event_datestart'],
-        "event_dateend": event['event_dateend']
-    })
-   
-    return response
+    try:
+        response = table.put_item(
+            Item= {
+            "event_id": event['event_id'],
+            "category_id":event['category_id'],
+            "event_name": event['event_name'],
+            "event_coordinates": event['event_coordinates'],
+            "event_images": event['event_images'],
+            "event_description": event['event_description'],
+            "event_range_age": event['event_range_age'],
+            "event_price": event['event_price'],
+            "event_slots": event['event_slots'],
+            "event_max": event['event_max'],
+            "event_datestart": event['event_datestart'],
+            "event_dateend": event['event_dateend']
+        })
+    
+        return response
+    except Exception as e:
+        print(str(e))
+        raise Exception(str(e))
 
 def validate(data):
     for name in data.keys():
@@ -57,15 +61,16 @@ def lambda_handler(event, context):
             "event_dateend": event['event_dateend']
         }
         validate(new_event)
-        
+        new_event = json.loads(json.dumps(new_event), parse_float=Decimal)
+        data = create_event(new_event)
+        response['error'] = False
+        response['data'] = data
+
     except Exception as e:
         response['error']  = True
         response['message'] = str(e).replace("'","")  + " missing"
-        return response
-    
-    new_event = json.loads(json.dumps(new_event), parse_float=Decimal)
-    data = create_event(new_event)
-    response['error'] = False
-    response['data'] = data
     
     return response
+        
+    
+    
