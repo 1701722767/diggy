da71f06a5a469933ea3786b92149af3b1f638da5
sergiomonz <sergio.1701510237@ucaldas.edu.co>
2022-05-20 16:24:13 -0500

    Update main.js

--- dashboard/src/main.js
+++ dashboard/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
-import router from './router'
+//import router from './router'
 import 'vuetify/dist/vuetify.min.css'
 
 // Import the functions you need from the SDKs you need
