f23b6061ee59eabf9fd6681a782e0a0797f9b849
sergiomonz <sergio.1701510237@ucaldas.edu.co>
2022-05-20 17:04:48 -0500

    Update router.js

--- dashboard/src/router.js
+++ dashboard/src/router.js
@@ -34,7 +34,7 @@ export default new Router({
         },
         {
           path: '/eventregister',
-          name: 'Event-register',
+          name: 'Eventregister',
           component: EventRegister
         },
         {
