a16822a2bfc73bd42be88daea68ae3a9b67496b4
sergiomonz <sergio.1701510237@ucaldas.edu.co>
2022-05-20 15:42:56 -0500

    Re-creating front components
    Recuperación de componentes Front y copia desde branch Main

---
+++ dashboard/src/components/map.vue
@@ -0,0 +1,29 @@
+<template>
+  <l-map style="height: 900px" :zoom="zoom" :center="center">
+    <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+    <l-marker :lat-lng="markerLatLng"></l-marker>
+  </l-map>
+</template>
+
+<script>
+import { latLng } from 'leaflet';
+import {LMap, LTileLayer, LMarker} from 'vue2-leaflet';
+
+export default {
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+  },
+  data () {
+    return {
+      url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      attribution:
+        '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      zoom: 16,
+      center: [5.05690, -75.50356],
+      markerLatLng:[5.05690, -75.50356]
+    };
+  }
+}
+</script>
