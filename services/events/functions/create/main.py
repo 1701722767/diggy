a6d79e2436d0042fefc12a7735686f2c1c303c6e
nicolascr181 <nicolas.1701625295@ucaldas.edu.co>
2022-05-20 21:23:54 -0500

    many more validations added & endpoint proxy lambda create events corrected

--- services/events/functions/create/main.py
+++ services/events/functions/create/main.py
@@ -1,22 +1,34 @@
 import json
 import boto3
 from decimal import Decimal
-from datetime import datetime
 from boto3.dynamodb.conditions import Key, Attr
 import uuid
 
 client = boto3.resource('dynamodb')
+
+# this will search for dynamoDB table 
 events_table = client.Table("events")
 categories_table = client.Table("categories")
+
+KEY_ERROR_MESSAGE = {
+    "category_id": "La categoría no existe o no fue seleccionada",
+    "user_id" : "No se indicó el usuario al cual le pertenece el evento",
+    "name": "Debe escribir el nombre del evento",
+    "coordinates": "Es necesaria la ubicación del evento",
+    "images": "El evento debe tener imagenes",
+    "description": "El evento debe tener una descripción",
+    "range_age": "El rango de edad debe ser selecionado",
+    "price": "Debe indicar el precio del evento",
+    "slots": "Debe indicar los cupos disponibles actuales del evento",
+    "max": "Debe ingresar la cantidad maxima de personas al evento",
+    "datestart": "Debe ingresar la hora y fecha de inicio del evento",
+    "dateend": "Debe ingresar la hora y fecha en la cual termina el evento"
+}
 
 # UUID, Universal Unique Identifier, is a python library which helps
 # in generating random objects of 128 bits as ids. 
 # It provides the uniqueness as it generates ids on the basis of time, Computer hardware (MAC etc.).
 def create_id():
-    
-    # # Generate a UUID from a host ID, sequence number, and the current time. If node is not given, 
-    # # getnode() is used to obtain the hardware address. If clock_seq is given, it is used as the sequence number; 
-    # # otherwise a random 14-bit sequence number is chosen.
     # new_id = "E" + str(uuid.uuid1())
     
     # Generate a random UUID.
@@ -25,7 +37,7 @@ def create_id():
     
     return new_id
 
-
+# Verify if category_id exists
 def exists(category_id):
     
     select = "SPECIFIC_ATTRIBUTES"
@@ -37,81 +49,111 @@ def exists(category_id):
         KeyConditionExpression=Key('category_id').eq(category_id)
     )
     if response['Count'] == 0:
-        raise Exception(f"category_id = {category_id} not found")
+        raise KeyError("category_id")
     
 def create_event(event):
     
-    # this will search for dynamoDB table 
-    
+    #this will create a random ID for event_id
     new_id = create_id()
     
+    
+    #this will try to find the selected category
     try:
         exists(event['category_id'])
-    except Exception as e:
-        print(str(e))
-        raise Exception(str(e))
+    except KeyError as e:
+        raise e
         
     
     try:
         response = events_table.put_item(
             Item= {
             "event_id": new_id,
+            "user_id": event['user_id'],
             "category_id":event['category_id'],
-            "event_name": event['event_name'],
-            "event_coordinates": event['event_coordinates'],
-            "event_images": event['event_images'],
-            "event_description": event['event_description'],
-            "event_range_age": event['event_range_age'],
-            "event_price": event['event_price'],
-            "event_slots": event['event_slots'],
-            "event_max": event['event_max'],
-            "event_datestart": event['event_datestart'],
-            "event_dateend": event['event_dateend']
+            "name": event['name'],
+            "coordinates": event['coordinates'],
+            "images": event['images'],
+            "description": event['description'],
+            "range_age": event['range_age'],
+            "price": event['price'],
+            "slots": event['slots'],
+            "max": event['max'],
+            "datestart": event['datestart'],
+            "dateend": event['dateend']
         })
-    
-        return response
+        validate_dynamodb_response(response)
+        
     except Exception as e:
         print(str(e))
-        raise Exception(str(e))
+        raise e
+    
+    return response
 
+#Verify all keys have a value
 def validate(data):
     for name in data.keys():
         if not data[name]:
             print(name," missing")
-            raise Exception(str(name))
+            raise KeyError(str(name))
+
+#Verify response from dynamodb
+def validate_dynamodb_response(response):
+    if response['ResponseMetadata']['HTTPStatusCode'] != 200:
+        print("Error al intentar crear un nuevo evento")
+        raise Exception("Error interno en la base de datos")
             
 def lambda_handler(event, context):
     # TODO implement
-    response = {
+    
+    message = {
         "error" : False,
-        "message": "Event was created successfully",
+        "message": "El evento fue creado exitosamente",
         "data": None
     }
     
+    response = {
+        'statusCode': 200,
+        'headers': {'Content-Type': 'application/json'},
+        'body': json.dumps(message)
+    }
+    
+    
+    
     try:
+        # ID User from the owner of the event
+        user_id = event["requestContext"]["authorizer"]["claims"]["sub"]
+        event_data = json.loads(event["body"], parse_float=Decimal)
         new_event = {
            
-            "category_id":event['category_id'],
-            "event_name": event['event_name'],
-            "event_coordinates": event['event_coordinates'],
-            "event_images": event['event_images'],
-            "event_description": event['event_description'],
-            "event_range_age": event['event_range_age'],
-            "event_price": event['event_price'],
-            "event_slots": event['event_slots'],
-            "event_max": event['event_max'],
-            "event_datestart": event['event_datestart'],
-            "event_dateend": event['event_dateend']
+            "category_id":event_data['category_id'],
+            "user_id": user_id,
+            "name": event_data['name'],
+            "coordinates": event_data['coordinates'],
+            "images": event_data['images'],
+            "description": event_data['description'],
+            "range_age": event_data['range_age'],
+            "price": event_data['price'],
+            "slots": event_data['slots'],
+            "max": event_data['max'],
+            "datestart": event_data['datestart'],
+            "dateend": event_data['dateend']
         }
         validate(new_event)
-        new_event = json.loads(json.dumps(new_event), parse_float=Decimal)
         data = create_event(new_event)
-        response['data'] = data
-
+       
+        
+    except KeyError as e:
+        message['error']  = True
+        message['message'] = KEY_ERROR_MESSAGE[e.args[0]]
+        response['statusCode'] = 500
+        
     except Exception as e:
-        response['error']  = True
-        response['message'] = str(e).replace("'","") + " missing" 
+        message['error']  = True
+        message['message'] = "Error interno en el servidor"
+        response['statusCode'] = 500
+        
     
+    response['body'] = json.dumps(message)
     return response
         
     
