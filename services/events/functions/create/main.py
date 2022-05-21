0f5f8c504f34f1153175fb4c31c7e72f90fee913
nicolascr181 <nicolas.1701625295@ucaldas.edu.co>
2022-05-21 16:06:54 -0500

    multiple unnecessary try-catch blocks deleted

--- services/events/functions/create/main.py
+++ services/events/functions/create/main.py
@@ -21,23 +21,13 @@ KEY_ERROR_MESSAGE = {
 }
 
 client = boto3.resource('dynamodb',region_name=AWS_REGION)
-# this will search for dynamoDB table 
 events_table = client.Table("events")
 categories_table = client.Table("categories")
 
-# UUID, Universal Unique Identifier, is a python library which helps
-# in generating random objects of 128 bits as ids. 
-# It provides the uniqueness as it generates ids on the basis of time, Computer hardware (MAC etc.).
-def create_id():
-    # new_id = "E" + str(uuid.uuid1())
-    
-    # Generate a random UUID.
-    # probability of collision really really small
-    new_id = "E" + str(uuid.uuid4())
-    
-    return new_id
 
-# Verify if category_id exists
+def create_id():    
+    return "E" + str(uuid.uuid4())
+
 def exists(category_id):
     
     select = "SPECIFIC_ATTRIBUTES"
@@ -52,59 +42,41 @@ def exists(category_id):
         raise KeyError("category_id")
     
 def create_event(event):
-    
-    #this will create a random ID for event_id
-    new_id = create_id()
-    
-    
-    #this will try to find the selected category
-    try:
-        exists(event['category_id'])
-    except KeyError as e:
-        raise e
+
+    exists(event['category_id'])
+    response = events_table.put_item(
+        Item= {
+        "event_id": create_id(),
+        "user_id": event['user_id'],
+        "category_id":event['category_id'],
+        "name": event['name'],
+        "coordinates": event['coordinates'],
+        "images": event['images'],
+        "description": event['description'],
+        "range_age": event['range_age'],
+        "price": event['price'],
+        "slots": event['slots'],
+        "max": event['max'],
+        "datestart": event['datestart'],
+        "dateend": event['dateend']
+    })
+    validate_dynamodb_response(response)
         
-    
-    try:
-        response = events_table.put_item(
-            Item= {
-            "event_id": new_id,
-            "user_id": event['user_id'],
-            "category_id":event['category_id'],
-            "name": event['name'],
-            "coordinates": event['coordinates'],
-            "images": event['images'],
-            "description": event['description'],
-            "range_age": event['range_age'],
-            "price": event['price'],
-            "slots": event['slots'],
-            "max": event['max'],
-            "datestart": event['datestart'],
-            "dateend": event['dateend']
-        })
-        validate_dynamodb_response(response)
-        
-    except Exception as e:
-        print(str(e))
-        raise e
-    
     return response
 
-#Verify all keys have a value
+
 def validate(data):
     for name in data.keys():
         if not data[name]:
-            print(name," missing")
             raise KeyError(str(name))
 
-#Verify response from dynamodb
+
 def validate_dynamodb_response(response):
     if response['ResponseMetadata']['HTTPStatusCode'] != 200:
-        print("Error al intentar crear un nuevo evento")
         raise Exception("Error interno en la base de datos")
             
 def lambda_handler(event, context):
-    # TODO implement
-    
+   
     message = {
         "error" : False,
         "message": "El evento fue creado exitosamente",
@@ -119,7 +91,7 @@ def lambda_handler(event, context):
     
     
     try:
-        # ID User from the owner of the event
+        
         user_id = event["requestContext"]["authorizer"]["claims"]["sub"]
         event_data = json.loads(event["body"], parse_float=Decimal)
         new_event = {
@@ -142,11 +114,13 @@ def lambda_handler(event, context):
        
         
     except KeyError as e:
+        print(e)
         message['error']  = True
         message['message'] = KEY_ERROR_MESSAGE[e.args[0]]
-        response['statusCode'] = 500
+        response['statusCode'] = 400
         
     except Exception as e:
+        print(e)
         message['error']  = True
         message['message'] = "Error interno en el servidor"
         response['statusCode'] = 500
