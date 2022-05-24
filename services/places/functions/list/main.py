2fffd6e9c9d47bdc917ef94ee77c90592c817c70
nicolascr181 <nicolas.1701625295@ucaldas.edu.co>
2022-05-23 21:44:04 -0500

    paginate corrections & tests

--- services/places/functions/list/main.py
+++ services/places/functions/list/main.py
@@ -1,35 +1,85 @@
 import json
 import boto3
+import base64
+from decimal import Decimal
 
-PAGE_SIZE = 15
+PAGE_SIZE = 5
 AWS_REGION = "us-east-1"
 
 dynamodb = boto3.resource('dynamodb',region_name=AWS_REGION)
 places_table= dynamodb.Table('places')
 
-def list_places():
-    response = places_table.scan(Limit = PAGE_SIZE)
+
+def list_places(event):
+    
+    queryStringParameters = event["queryStringParameters"]
+    
+    items = {
+        'Items' : []
+    }
+    
+    if ((queryStringParameters) and ('last_evaluated_key' in queryStringParameters) and
+        (queryStringParameters['last_evaluated_key'])):
+            
+        last_evaluated_key = queryStringParameters['last_evaluated_key']
+        response = places_table.scan(
+            Limit = PAGE_SIZE,
+            ExclusiveStartKey = decodeBase64ToJson(last_evaluated_key)
+        )
+        
+    else:
+        response = places_table.scan(
+            Limit = PAGE_SIZE,
+        )
+    
+    items['Items'] = response['Items']
+    
+    if 'LastEvaluatedKey' in response:
+        items['last_evaluated_key'] = encodeJSONToBase64(response['LastEvaluatedKey'])
+        
     validate_dynamodb_response(response)
-    return response['Items']
+    
+    return items
+    
+    
+def encodeJSONToBase64(jsonData):
+    stringJSON = json.dumps(jsonData)
+    json_bytes = stringJSON.encode('ascii')
+    base64_bytes = base64.b64encode(json_bytes)
+    return base64_bytes.decode('ascii')
+
+def decodeBase64ToJson(base64Data):
+    base64_bytes = base64Data.encode('ascii')
+    message_bytes = base64.b64decode(base64_bytes)
+    return json.loads(message_bytes.decode('ascii'), parse_float=Decimal)
     
 def validate_dynamodb_response(response):
     if response['ResponseMetadata']['HTTPStatusCode'] != 200:
         raise Exception("Error interno")
 
 def lambda_handler(event, context):
-    # TODO implement
     
-    response = {
+    message = {
         "error" : False,
         "message" : "Lugares listados correctamente",
         "data":None
     }
     
+    response =  {
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'application/json'
+        },
+        'body' : None
+    }
+    
     try:
-        response['data'] = list_places()
+        message['data'] = list_places(event)
     except Exception as e:
         print(e)
-        response['error'] = True
-        response['message'] = "Error interno del servidor"
+        response['statusCode'] = 500
+        message['error'] = True
+        message['message'] = "Error interno del servidor"
     
+    response['body'] = json.dumps(message,default=str,ensure_ascii=False)
     return response
--- services/places/functions/list/main_test.py
+++ services/places/functions/list/main_test.py
@@ -1,6 +1,49 @@
 import pytest
 import main
 
+
+class TableMock():
+    def scan(self,Limit, ExclusiveStartKey=None):
+
+        places = [
+            {'name' : 'PollosMarios','id' : 'P01'},
+            {'name' : 'PollosJC','id' : 'P02'}
+        ]
+
+        response = {
+            'ResponseMetadata' :{
+                'HTTPStatusCode': 200
+            },
+            'Items': places,
+            'LastEvaluatedKey' : 'P01'
+        }
+
+        if(ExclusiveStartKey and (ExclusiveStartKey != places[0]['id'])):
+            response['ResponseMetadata']['HTTPStatusCode'] = 500
+            response['Items'] = []
+
+        return response
+
+@pytest.mark.parametrize(
+    "input,expected",[
+        ({ 'queryStringParameters' : None},
+        '{"error": false, "message": "Lugares listados correctamente", "data": {"Items": [{"name": "PollosMarios", "id": "P01"}, {"name": "PollosJC", "id": "P02"}], "last_evaluated_key": "IlAwMSI="}}'
+        ),
+        ({ 'queryStringParameters' : {
+            'last_evaluated_key': 'IlAwMSI='}},
+        '{"error": false, "message": "Lugares listados correctamente", "data": {"Items": [{"name": "PollosMarios", "id": "P01"}, {"name": "PollosJC", "id": "P02"}], "last_evaluated_key": "IlAwMSI="}}'
+        ),
+        ({ 'queryStringParameters' : {
+            'last_evaluated_key': 'P01'}},
+        '{"error": true, "message": "Error interno del servidor", "data": null}'
+        )
+    ]
+)
+def test_lambda_handler(input,expected):
+    main.places_table = TableMock()
+    response =  main.lambda_handler(input,{})
+    assert response['body'] == expected
+
 @pytest.mark.parametrize(
     "input",[
         (400),
@@ -11,3 +54,6 @@ import main
 def test_validate_dynamodb_response_failure(input):
     with pytest.raises(Exception) as e:
         main.validate_dynamodb_response(input)
+
+
+
