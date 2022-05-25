6135963400be189374b10043df01611da66be90e
sergiomonz <sergio.1701510237@ucaldas.edu.co>
2022-05-24 22:15:07 -0500

    Fixing marker icon

--- dashboard/src/components/Events/Register.vue
+++ dashboard/src/components/Events/Register.vue
@@ -174,6 +174,14 @@
 
 <script>
 import { LMap, LTileLayer, LMarker } from "vue2-leaflet";
+import { Icon } from 'leaflet';
+
+delete Icon.Default.prototype._getIconUrl;
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
 
 export default {
   name: "EventRegister",
