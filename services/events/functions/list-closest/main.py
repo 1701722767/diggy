d1b8656f6f1baa5a9389d92af141d1141e4481be
nicolascr181 <nicolas.1701625295@ucaldas.edu.co>
2022-06-18 17:02:28 -0500

    get closest events by lat and lng

---
+++ services/events/functions/list-closest/main.py
@@ -0,0 +1,80 @@
+import json
+import boto3
+import base64
+from decimal import Decimal
+from math import radians, cos, sin, asin, sqrt
+
+AWS_REGION = "us-east-1"
+dynamodb = boto3.resource('dynamodb', region_name=AWS_REGION)
+events_table = dynamodb.Table('events')
+MIN_DISTANCE = 900
+
+def get_closest_events(center_coordinates):
+    
+    response = events_table.scan()
+    
+    if 'Items'not in response or not response['Items']:
+        raise Exception("No existe ningún evento registrado")
+    
+    events = response['Items']
+    
+    closest_events = []
+    
+    for event in events:
+        if distance(center_coordinates,event['coordinates']) <= MIN_DISTANCE:
+            closest_events.append(event)
+             
+    return closest_events
+    
+def distance(coordinates1,coordinates2):
+    
+	lon1 = radians(coordinates1['longitude'])
+	lon2 = radians(coordinates2['longitude'])
+	lat1 = radians(coordinates1['latitude'])
+	lat2 = radians(coordinates2['latitude'])
+	dlon = lon2 - lon1
+	dlat = lat2 - lat1
+	a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+	c = 2 * asin(sqrt(a))
+	earth_radio = 6371
+	return(c * earth_radio)*1000
+
+def decodeBase64ToJson(base64Data):
+    base64_bytes = base64Data.encode('ascii')
+    message_bytes = base64.b64decode(base64_bytes)
+    return json.loads(message_bytes.decode('ascii'), parse_float=Decimal)
+	
+	
+def lambda_handler(event, context):
+    
+    message = {
+        "error" : False,
+        "message": "Eventos cercanos encontrados exitosamente",
+        "data" : {
+            "items" : None
+        }
+    }
+    
+    response = {
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': 'Content-Type',
+        },
+        'body': json.dumps(message)
+    }
+    
+    try:
+        base64_coordinates = event['queryStringParameters']['center_coordinates']
+        center_coordinates = decodeBase64ToJson(base64_coordinates)
+        message['data']['items'] = get_closest_events(center_coordinates)
+        
+    except Exception as e:
+        message['error'] = True
+        response['statusCode'] = 500
+        message['message'] = str(e)
+    
+    response['body'] = json.dumps(message,default=str,ensure_ascii=False,)
+    return response
+    
---
+++ services/events/functions/list-closest/main_test.py
@@ -0,0 +1,43 @@
+import pytest
+import main
+
+class TableEventMock():
+    def scan(self):
+        return {
+            "Items":[
+                {
+                    "coordinates" : {
+                        "latitude" : 9.10,
+                        "longitude" : 8.5
+                    }
+                },
+                {
+                    "coordinates" : {
+                        "latitude" : 9.10,
+                        "longitude" : 8.5
+                    }
+                }
+            ]
+        }
+
+
+@pytest.mark.parametrize(
+    "input,expected",[
+        (
+            {
+                "queryStringParameters": 
+                {
+                    "center_coordinates": "ew0KICAgICJsYXRpdHVkZSIgOiA1My4zMTg2MTExMTExMTExMSwNCiAgICAibG9uZ2l0dWRlIiA6IC0xLjY5OTcyMjIyMjIyMjIyMjMNCn0="
+                }
+            },
+            '{"error": false, "message": "Eventos cercanos encontrados exitosamente", "data": {"items": []}}'
+        )
+    ]
+)
+def test_lambda_handler(input,expected):
+    main.events_table = TableEventMock()
+    response = main.lambda_handler(input,{})
+    print(response['body'])
+    assert response['body'] == expected
+        
+
